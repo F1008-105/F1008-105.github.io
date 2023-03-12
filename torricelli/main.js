5c2a2a59b6a5ed1ca18c7b7ee9b3d92c24c66abd
Andrés Martínez <autummata@gmail.com>
2023-03-11 22:18:37 -0700

    Added back button

--- torricelli/main.js
+++ torricelli/main.js
@@ -1,19 +1,25 @@
 // Set up the bottle and valve
-var pos_bx = 225, pos_by =225;
-var b_base = 76.2, b_altura = 165.1;
-var raise_val = 25.4;
-var base_val = 10, altura_val = 5;
+let pos_bx = 225,
+  pos_by = 225;
+let b_base = 76.2,
+  b_altura = 165.1;
+let raise_val = 25.4;
+let base_val = 10,
+  altura_val = 5;
+
 
 // Set up the water
-var aguah = b_altura;
-var base_area = Math.PI * Math.pow(b_base / 2, 2);
-var agua_vd = base_area * aguah;
+let aguah = b_altura;
+let base_area = Math.PI * Math.pow(b_base / 2, 2);
+let agua_vd = base_area * aguah;
 
 // Set up the animation parameters
-var ti = 0, tf = 50, n = 100;
-var dt = (tf - ti) / n;
-var t = 0;
-var vel, G;
+let ti = 0,
+  tf = 50,
+  n = 100;
+let dt = (tf - ti) / n;
+let t = 0;
+let vel, G;
 
 function draw() {
   // Calculate the new water level and velocity
@@ -26,10 +32,27 @@ function draw() {
   // Clear the screen and draw the objects
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "blue";
+  ctx.fillStyle = "#99bbff";
   ctx.fillRect(pos_bx, canvas.height - pos_by - aguah, b_base, aguah);
-  ctx.fillRect(pos_bx + b_base, canvas.height - pos_by - raise_val - altura_val, base_val, altura_val);
-  
+
+  // Draw the valve
+  ctx.fillRect(
+    pos_bx + b_base,
+    canvas.height - pos_by - raise_val - altura_val,
+    base_val,
+    altura_val
+  );
+
+  // contorno
+  ctx.strokeRect(
+    pos_bx + b_base,
+    canvas.height - pos_by - raise_val - altura_val,
+    base_val,
+    altura_val
+  );
+
+  ctx.strokeRect(pos_bx, 110, b_base, b_altura);
+
   // Draw the final water level and graph
 
   t += dt;
@@ -39,7 +62,10 @@ function draw() {
 // Set up the canvas
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 600;
+
+const CANVAS_WIDTH = 500;
+const CANVAS_HEIGHT = 500;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 
 draw();
