7c5ecfb3f46fa0e1115fa7976f0c69062c7db243
Andrés Martínez <autummata@gmail.com>
2023-03-13 16:15:44 -0700

    updated histogram

---
+++ histogram/hist_mariotte2.js
@@ -0,0 +1,53 @@
+// Conseguir el elemento del canvas
+var ctx = document.getElementById('hist_Mariotte2').getContext('2d');
+
+var data = {
+    labels: ['260', '280', '300', '320', '340'],
+    datasets: [
+      {
+        label: 'Frecuencia',
+        data: [1, 2, 3, 2, 1],
+        backgroundColor: 'rgba(255, 222, 55, 0.2)',
+        borderColor: 'rgba(255, 222, 55, 1)',
+        borderWidth: 1,
+        yAxisID: 'left-y-axis'
+      },
+      {
+        label: 'Desviacion estandar',
+        data: [0.5, 1.5, 2.5, 1.5, 0.5],
+        type: 'line',
+        fill: false,
+        borderColor: 'rgba(54, 162, 235, 1)',
+        borderWidth: 1,
+        pointRadius: 0,
+        yAxisID: 'left-y-axis',
+        lineTension: 0.4
+      }
+    ]
+  };
+  var options = {
+    scales: {
+      xAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }],
+      yAxes: [
+        {
+          id: 'left-y-axis',
+          type: 'linear',
+          position: 'left',
+          ticks: {
+            beginAtZero: true
+          },
+          stacked: true
+        }]
+    }
+  };
+  var hist_Mariotte = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: options
+  });
+  
+  
--- histogram/hist_torricelli.js
+++ histogram/hist_torricelli.js
@@ -4,13 +4,26 @@ var ctx = document.getElementById('hist_Torricelli').getContext('2d');
 // Definar la data para el histograma
 var data = {
   labels: ['26', '28', '30', '32', '34'],
-  datasets: [{
+  datasets: [
+    {
     label: 'Frecuencia',
     data: [0, 1, 2, 1, 0],
-    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+    backgroundColor: 'rgba(255, 222, 55, 0.2)',
+    borderColor: 'rgba(255, 222, 55, 1)',
+    borderWidth: 1,
+    yAxisID: 'axis_izq'
+  }, {
+    label: 'Desviacion estandar',
+    data: [0.5, 1.5, 2.5, 1.5, 0.5],
+    type: 'line',
+    fill: false,
     borderColor: 'rgba(54, 162, 235, 1)',
-    borderWidth: 1
-  }]
+    borderWidth: 1,
+    pointRadius: 0,
+    yAxisID: 'axis_izq',
+    lineTension: 0.4
+  }
+]
 };
 
 // Define las opciones para el histograma
@@ -20,7 +33,17 @@ var options = {
       ticks: {
         beginAtZero: true
       }
-    }]
+    }],
+    yAxes: [
+      {
+        id:'axis_izq',
+        type: 'linear',
+        position: 'left',
+        ticks: {
+          beginAtZero: true
+        },
+        stacked: true
+      }]
   }
 };
 
