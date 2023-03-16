2b4c89eee3bef7c5c5ea658ddb19b4762e0bb22c
Andrés Martínez <autummata@gmail.com>
2023-03-15 19:14:53 -0700

    Added start button

--- torricelli/main.js
+++ torricelli/main.js
@@ -20,64 +20,73 @@ let ti = 0,
 let dt = (tf - ti) / n;
 let t = 0;
 let vel, G;
-
-let flow = true; 
-
-function draw() {
-  // Calculate the new water level and velocity
-  if (aguah > raise_val + altura_val && flow) {
-    vel = Math.sqrt(2 * 9.8 * aguah);
-    G = vel * dt * Math.pow(altura_val / 2, 2) * Math.PI;
-    agua_vd -= G;
-    aguah = agua_vd / base_area;
-  }
-  // Clear the screen and draw the objects
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "#99bbff";
-  ctx.fillRect(pos_bx, canvas.height - pos_by - aguah, b_base, aguah);
-
-  // Draw the valve
-  ctx.fillRect(
-    pos_bx + b_base,
-    canvas.height - pos_by - raise_val - altura_val,
-    base_val,
-    altura_val
-  );
-
-  // contorno
-  ctx.strokeRect(
-    pos_bx + b_base,
-    canvas.height - pos_by - raise_val - altura_val,
-    base_val,
-    altura_val
-  );
-
-  ctx.strokeRect(pos_bx, 110, b_base, b_altura);
-
-
-  // Draw the final water level and graph
-
-  t += dt;
-  setTimeout(draw, 10);
-}
-
 let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext("2d");
+let elementsToRender = document.getElementById("elementsToRender");
 
-const title = document.getElementById("title");
+canvas.style.display = "none";
+let flow = true;
+let startButton = document.getElementById("start-button");
+elementsToRender.style.display = "none";
 
-let stopButton = document.getElementById("stop-button"); 
-stopButton.addEventListener("click", function() { 
-  console.log(":D");
-  flow = !flow; 
-  stopButton.style.backgroundColor = flow ? "#f8df85" : "#f7633e";
+// When pressing start: 
+startButton.addEventListener("click", function () {
+  canvas.style.display = "block";
+  startButton.style.display = "none";
+  elementsToRender.style.display = "block";
 
-}); 
+  function draw() {
+    // Calculate the new water level and velocity
+    if (aguah > raise_val + altura_val && flow) {
+      vel = Math.sqrt(2 * 9.8 * aguah);
+      G = vel * dt * Math.pow(altura_val / 2, 2) * Math.PI;
+      agua_vd -= G;
+      aguah = agua_vd / base_area;
+    }
+    // Clear the screen and draw the objects
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#99bbff";
+    ctx.fillRect(pos_bx, canvas.height - pos_by - aguah, b_base, aguah);
 
-const CANVAS_WIDTH = 500;
-const CANVAS_HEIGHT = 500;
-canvas.width = CANVAS_WIDTH;
-canvas.height = CANVAS_HEIGHT;
+    // Draw the valve
+    ctx.fillRect(
+      pos_bx + b_base,
+      canvas.height - pos_by - raise_val - altura_val,
+      base_val,
+      altura_val
+    );
 
-draw();
+    // contorno
+    ctx.strokeRect(
+      pos_bx + b_base,
+      canvas.height - pos_by - raise_val - altura_val,
+      base_val,
+      altura_val
+    );
+
+    ctx.strokeRect(pos_bx, 110, b_base, b_altura);
+
+    // Draw the final water level and graph
+
+    t += dt;
+    setTimeout(draw, 10);
+  }
+
+  let ctx = canvas.getContext("2d");
+
+  const title = document.getElementById("title");
+
+  let stopButton = document.getElementById("stop-button");
+  stopButton.addEventListener("click", function () {
+    console.log(":D");
+    flow = !flow;
+    stopButton.style.backgroundColor = flow ? "#f8df85" : "#f7633e";
+  });
+
+  const CANVAS_WIDTH = 500;
+  const CANVAS_HEIGHT = 500;
+  canvas.width = CANVAS_WIDTH;
+  canvas.height = CANVAS_HEIGHT;
+
+  draw();
+});
